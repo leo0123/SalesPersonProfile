734ece1965916df8fed4a7fc074c52a186bb0217
Leo <leo.cc0123@gmail.com>
2017-10-25 15:12:43 -0700

    auto url

--- AdjustmentConfig.js
+++ AdjustmentConfig.js
@@ -1,6 +1,6 @@
 var myAdjustmentConfig = myAdjustmentConfig || {};
 myAdjustmentConfig.myFormUrl = '/sites/MyDelta/SiteAssets/SPP/MyAdjustmentForm.html';
-myAdjustmentConfig.SPServerUrl = "http://ideltaam.deltaww.com/sites/MyDelta/";
+myAdjustmentConfig.SPServerUrl = _spPageContextInfo.siteAbsoluteUrl + "/";
 myAdjustmentConfig.dataServiceUrl = "http://amdpfweb02:9999/SAPBW3DataService.svc/";
-myAdjustmentConfig.AdjustmentListUrl = myAdjustmentConfig.SPServerUrl + "_api/web/lists/GetByTitle('Sales Revenue Adjustment')/items";
+myAdjustmentConfig.AdjustmentListUrl = myAdjustmentConfig.SPServerUrl + "_api/web/lists/GetById('" + _spPageContextInfo.pageListId + "')/items";
 console.log("local config");
